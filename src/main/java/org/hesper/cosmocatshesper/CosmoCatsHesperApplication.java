3f175d62eb070cc5d9109be495e88c0460c50e59
Hesperok <jaldak1983@gmail.com>
2026-01-20 23:20:57 +0200

    chore: initial commit

---
+++ src/main/java/org/hesper/cosmocatshesper/CosmoCatsHesperApplication.java
@@ -0,0 +1,13 @@
+package org.hesper.cosmocatshesper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoCatsHesperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmoCatsHesperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hesper/cosmocatshesper/CosmoCatsHesperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hesper.cosmocatshesper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoCatsHesperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
